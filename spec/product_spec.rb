874932c86ae3efa380cd4d459b54595b82a41ac9
Kris Hopper <hopperdavis@gmail.com>
2021-08-06 09:27:54 -0700

    WIP: shoulda-matchers not working properly

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe Product do
   it { should have_many(:reviews) }
 
-  it { should validate_presence_of :name, :cost, :country_of_origin }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost } 
+  it { should validate_presence_of :country_of_origin }
 
   it("titleizes the name of a product") do
     product = Product.create({name:"lasagna", cost: 9.99, country_of_origin: "Italy"})
